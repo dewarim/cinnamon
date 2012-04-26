65d347ca9b82e1f20f52c4c4ee2c749bed6e862b
Ingo Wiarda <ingo_wiarda@dewarim.de>
2012-04-26 14:43:13 +0200

    Added generic test file

---
+++ test/unit/cinnamon/CinnamonControllerTests.groovy
@@ -0,0 +1,17 @@
+package cinnamon
+
+
+
+import grails.test.mixin.*
+import org.junit.*
+
+/**
+ * See the API for {@link grails.test.mixin.web.ControllerUnitTestMixin} for usage instructions
+ */
+@TestFor(CinnamonController)
+class CinnamonControllerTests {
+
+    void testSomething() {
+        fail "Implement me"
+    }
+}
