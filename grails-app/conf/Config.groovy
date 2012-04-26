917cbbd3bee91a295e17ba40a5f106837d29a353
Ingo Wiarda <ingo_wiarda@dewarim.de>
2012-04-26 14:07:51 +0200

    Changed default encoding method to HTML (safer than adding raw $value to output)

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -37,7 +37,7 @@ grails.resources.adhoc.patterns = ['/images/*', '/css/*', '/js/*', '/plugins/*']
 
 
 // The default codec used to encode data with ${}
-grails.views.default.codec = "none" // none, html, base64
+grails.views.default.codec = "html" // none, html, base64
 grails.views.gsp.encoding = "UTF-8"
 grails.converters.encoding = "UTF-8"
 // enable Sitemesh preprocessing of GSP pages
@@ -91,4 +91,6 @@ log4j = {
            'org.springframework',
            'org.hibernate',
            'net.sf.ehcache.hibernate'
+    debug   'cinnamon'
+
 }
