8b97121abe708c62b1497670ad26571e890c791a
Ingo Wiarda <ingo_wiarda@dewarim.de>
2012-04-27 14:32:54 +0200

    + load config if needed + initialize luceneService + fix position of RepositoryLoginFilter in security filter chain

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,7 +1,28 @@
+import org.codehaus.groovy.grails.plugins.springsecurity.SpringSecurityUtils
+import org.codehaus.groovy.grails.plugins.springsecurity.SecurityFilterPosition
+
 class BootStrap {
 
+    def grailsApplication
+    def luceneService
+    
     def init = { servletContext ->
+
+
+        if (!grailsApplication.config.configLoaded) {
+            log.warn("merge config file by hand")
+            def configFile = new File("${System.env.CINNAMON_HOME_DIR}/cinnamon-config.groovy")
+            def configScript = new ConfigSlurper().parse(configFile.text)
+            grailsApplication.config.merge(configScript)
+        }
+
+        SpringSecurityUtils.clientRegisterFilter('repositoryLoginFilter', 
+                SecurityFilterPosition.PRE_AUTH_FILTER.getOrder() + 20 )
+
+        luceneService.initialize()
     }
+    
     def destroy = {
+        
     }
 }
