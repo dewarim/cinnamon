a4b5ff9663fbf49747b7ccb8c0e9e13899b7cbc3
Ingo Wiarda <ingo_wiarda@dewarim.de>
2012-04-27 14:39:08 +0200

    Moved Spring-Security-Core plugin from cinnamonBase to cinnamon module.

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -51,6 +51,7 @@ grails.project.dependency.resolution = {
         runtime ":hibernate:$grailsVersion"
         runtime ":jquery:1.7.1"
         runtime ":resources:1.1.6"
+        compile(':spring-security-core:1.2.7.2')
 
         // Uncomment these (or add new ones) to enable additional resources capabilities
         //runtime ":zipped-resources:1.0"
@@ -58,6 +59,6 @@ grails.project.dependency.resolution = {
         //runtime ":yui-minify-resources:0.1.4"
 
         build ":tomcat:$grailsVersion"
-        compile 'cinnamon:cinnamon-db:0.1.16'
+        compile 'cinnamon:cinnamon-db:0.1.29'
     }
 }
