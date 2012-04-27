aa5dcf4fdb802860515d3a6bc1037b55d3fa3da6
Ingo Wiarda <ingo_wiarda@dewarim.de>
2012-04-27 14:36:28 +0200

    fixed: do not create-drop the database during development

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -1,8 +1,8 @@
 dataSource {
     pooled = true
-    driverClassName = "org.h2.Driver"
-    username = "sa"
-    password = ""
+//    driverClassName = "org.h2.Driver"
+//    username = "sa"
+//    password = ""
 }
 hibernate {
     cache.use_second_level_cache = true
@@ -13,20 +13,20 @@ hibernate {
 environments {
     development {
         dataSource {
-            dbCreate = "create-drop" // one of 'create', 'create-drop', 'update', 'validate', ''
-            url = "jdbc:h2:mem:devDb;MVCC=TRUE"
+            dbCreate = "update" // one of 'create', 'create-drop', 'update', 'validate', ''
+//            url = "jdbc:h2:mem:devDb;MVCC=TRUE"
         }
     }
     test {
         dataSource {
             dbCreate = "update"
-            url = "jdbc:h2:mem:testDb;MVCC=TRUE"
+//            url = "jdbc:h2:mem:testDb;MVCC=TRUE"
         }
     }
     production {
         dataSource {
-            dbCreate = "update"
-            url = "jdbc:h2:prodDb;MVCC=TRUE"
+            dbCreate = "none"
+//            url = "jdbc:h2:prodDb;MVCC=TRUE"
             pooled = true
             properties {
                maxActive = -1
