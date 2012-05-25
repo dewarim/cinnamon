f3f53ce83d4a5f6f645dacda3d3ff909362724ab
Ingo Wiarda <ingo_wiarda@dewarim.de>
2012-05-25 12:15:24 +0200

    version++

--- CinnamonGrailsPlugin.groovy
+++ CinnamonGrailsPlugin.groovy
@@ -3,7 +3,7 @@ class CinnamonGrailsPlugin {
 //    def packaging = "binary"
     def groupId = 'cinnamon'
     // the plugin version
-    def version = "0.0.2"
+    def version = "0.0.5"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "2.0 > *"
     // the other plugins this plugin depends on
