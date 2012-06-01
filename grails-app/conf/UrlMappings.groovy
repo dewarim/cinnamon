924478755e7ffc8703e66c4871c7430a8a6756c8
Ingo Wiarda <ingo_wiarda@dewarim.de>
2012-06-01 16:26:50 +0200

    add default URL mapping: if no controller is requested, return XML list of repositories (used for legacy clients)

--- grails-app/conf/UrlMappings.groovy
+++ grails-app/conf/UrlMappings.groovy
@@ -1,13 +1,14 @@
 class UrlMappings {
 
 	static mappings = {
+                
 		"/$controller/$action?/$id?"{
 			constraints {
 				// apply constraints here
 			}
 		}
 
-		"/"(view:"/index")
+		"/"(controller: "cinnamon", action: "index")
 		"500"(view:'/error')
 	}
 }
