d94c0752ebad6da9f038e2460afe88ecb857c91d
Ingo Wiarda <ingo_wiarda@dewarim.de>
2012-06-27 17:58:03 +0200

    Change default home link to /folder/index.

--- grails-app/taglib/cinnamon/taglib/ButtonTagLib.groovy
+++ grails-app/taglib/cinnamon/taglib/ButtonTagLib.groovy
@@ -1,6 +1,6 @@
 package cinnamon.taglib
 class ButtonTagLib {
 	def homeButton = { attrs, body ->
-		out << """<span class="menuButton"><a class="home" href="${resource(dir:'security', file:'index')}">""" << body() << "</a></span>"
+		out << """<span class="menuButton"><a class="home" href="${resource(dir:'folder', file:'index')}">""" << body() << "</a></span>"
 	}
 }
