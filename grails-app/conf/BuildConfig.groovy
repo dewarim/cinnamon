4e07a06b8cd225f664a09f3423914c0ddfb195b8
Ingo Wiarda <ingo_wiarda@dewarim.de>
2012-09-28 15:42:02 +0200

    updated build script (versions++, exclude xml-apis from build)

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -12,14 +12,16 @@ grails.project.dependency.resolution = {
     inherits("global") {
         // uncomment to disable ehcache
         // excludes 'ehcache'
+        excludes "xercesImpl", "xmlParserAPIs", "xml-apis";
     }
     log "warn" // log level of Ivy resolver, either 'error', 'warn', 'info', 'debug' or 'verbose'
     checksums true // Whether to verify checksums on resolve
 
     repositories {
         inherits true // Whether to inherit repository definitions from plugins
-        mavenLocal()
         mavenRepo name:'myRepo'
+        mavenLocal()
+       
         grailsRepo "http://grails.org/plugins"
         mavenCentral()
         
@@ -47,7 +49,7 @@ grails.project.dependency.resolution = {
         runtime 'dom4j:dom4j:1.6.1'
         runtime 'jaxen:jaxen:1.1-beta-6'
         compile 'org.codehaus.gpars:gpars:1.0-beta-3'
-        runtime 'cinnamon:humulus:0.1.0'
+        runtime 'cinnamon:humulus:0.1.1'
     }
 
     plugins {
@@ -55,7 +57,7 @@ grails.project.dependency.resolution = {
         compile ":release:2.0.4"
         build ":tomcat:$grailsVersion"
         
-        runtime ":jquery:1.8.2"
+        runtime ":jquery:1.8.0"
         runtime ":resources:1.1.6"
         compile(':spring-security-core:1.2.7.3')
 
@@ -64,7 +66,7 @@ grails.project.dependency.resolution = {
         //runtime ":cached-resources:1.0"
         //runtime ":yui-minify-resources:0.1.4"
 
-        runtime 'cinnamon:cinnamon-db:0.2.32'
+        runtime 'cinnamon:cinnamon-db:0.2.35'
         compile ":remote-pagination:0.3"
         test ':spock:0.6'
     }
