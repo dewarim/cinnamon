53730b5acca19e518062760850bcfcf88127c91a
Ingo Wiarda <ingo_wiarda@dewarim.de>
2012-10-02 19:27:14 +0200

    Exclude strang groovy versions from breaking the cinnamon.war.

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -12,7 +12,7 @@ grails.project.dependency.resolution = {
     inherits("global") {
         // uncomment to disable ehcache
         // excludes 'ehcache'
-        excludes "xercesImpl", "xmlParserAPIs", "xml-apis";
+        excludes "xercesImpl", "xmlParserAPIs", "xml-apis", 'groovy';
     }
     log "warn" // log level of Ivy resolver, either 'error', 'warn', 'info', 'debug' or 'verbose'
     checksums true // Whether to verify checksums on resolve
@@ -50,6 +50,9 @@ grails.project.dependency.resolution = {
         runtime 'jaxen:jaxen:1.1-beta-6'
         compile 'org.codehaus.gpars:gpars:1.0-beta-3'
         runtime 'cinnamon:humulus:0.1.1'
+        compile("org.codehaus.groovy.modules.http-builder:http-builder:0.5.2"){
+            excludes "groovy"
+        }
     }
 
     plugins {
@@ -66,8 +69,10 @@ grails.project.dependency.resolution = {
         //runtime ":cached-resources:1.0"
         //runtime ":yui-minify-resources:0.1.4"
 
-        runtime 'cinnamon:cinnamon-db:0.2.35'
+        runtime 'cinnamon:cinnamon-db:0.2.36'
         compile ":remote-pagination:0.3"
-        test ':spock:0.6'
+        test (':spock:0.6'){
+            excludes: 'groovy'
+        }
     }
 }
