a22de80423fc9c7e573d11fdb341f7a3661273ba
Ingo Wiarda <ingo_wiarda@dewarim.de>
2013-05-03 14:44:03 +0200

    update to 3.0.0.27 of cinnamon-db

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -81,7 +81,7 @@ grails.project.dependency.resolution = {
         runtime ":resources:1.2"
         compile(':spring-security-core:1.2.7.3')
         compile (":twitter-bootstrap:2.1.0.1"){excludes 'svn'}
-        runtime ':cinnamon-db:3.0.0.26'
+        runtime ':cinnamon-db:3.0.0.27'
         runtime ':tika-parser:1.3.0.1'
         compile ":remote-pagination:0.3"
         compile ":geb:${gebPluginVersion}"
