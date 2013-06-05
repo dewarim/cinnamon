734f40ed1753eacf7abc3218c53b65cdfc9b9992
Ingo Wiarda <ingo_wiarda@dewarim.de>
2013-06-05 14:21:41 +0200

    Add checkout script for Cinnamon 3

---
+++ checkout.sh
@@ -0,0 +1,10 @@
+echo "Clone all git repositories of Cinnamon Server v3 from Github."
+
+# the core system
+git clone git://github.com/dewarim/cinnamon.git
+
+# domain classes and services
+git clone git://github.com/dewarim/cinnamon-db.git
+
+# multiple repository authentication and security
+git clone git://github.com/dewarim/cinnamon-humulus.git
