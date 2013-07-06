b31fe457c8cd3b890d1cd0f2daa8ebe6eb6ee754
Ingo Wiarda <ingo_wiarda@dewarim.de>
2013-07-06 20:41:29 +0200

    version++, because the next published version should not only differ in the build number (4th place).

--- CinnamonGrailsPlugin.groovy
+++ CinnamonGrailsPlugin.groovy
@@ -2,7 +2,7 @@ import cinnamon.Session
 
 class CinnamonGrailsPlugin {
 
-    def version = "3.0.0.44"
+    def version = "3.0.1.0"
     def grailsVersion = "2.2 > *"
     def dependsOn = [:]
     def pluginExcludes = [
