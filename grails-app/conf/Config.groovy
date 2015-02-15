b265beeca48e771ff613713d9217bcfcf6a76c59
Ingo Wiarda <ingo_wiarda@dewarim.de>
2015-02-15 10:24:16 +0100

    Remove option to use properties files for the main configuration: there are no example files or documentation in the Cinnamon docs for this use case and it's not recommended anyway.

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -1,17 +1,10 @@
 // locations to search for config files that get merged into the main config
 // config files can either be Java properties files or ConfigSlurper scripts
 
-grails.config.locations = ["classpath:${appName}-config.properties",
-                           "classpath:${appName}-config.groovy",
-                           "file:${userHome}/.grails/${appName}-config.properties",
+grails.config.locations = ["classpath:${appName}-config.groovy",
                            "file:${userHome}/.grails/${appName}-config.groovy",
                            "file:${System.env.CINNAMON_HOME_DIR}/${appName}-config.groovy"
 ]
-
-// if (System.properties["${appName}.config.location"]) {
-//    grails.config.locations << "file:" + System.properties["${appName}.config.location"]
-// }
-
 
 grails.project.groupId = appName // change this to alter the default package name and Maven publishing destination
 grails.mime.file.extensions = true // enables the parsing of file extensions from URLs into the request format
