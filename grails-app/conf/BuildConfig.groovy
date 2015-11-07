d4550bd8fca14a8e03f140df7ef0e89d58d7fb03
ingo.wiarda <ingo_wiarda@dewarim.de>
2015-11-07 15:04:20 +0100

    Move humulus module content to cinnamon-db.
     Fixes #84

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -39,7 +39,7 @@ grails.project.dependency.resolution = {
         }
         runtime 'jaxen:jaxen:1.1.4'
         compile 'org.codehaus.gpars:gpars:1.2.1'
-        runtime 'org.grails.plugins:cinnamon-humulus:0.3'
+//        runtime 'org.grails.plugins:cinnamon-humulus:0.3'
         runtime 'commons-net:commons-net:3.2'
         compile 'javax.mail:mail:1.4.7'
         test "org.gebish:geb-spock:${gebPluginVersion}" 
@@ -73,7 +73,7 @@ grails.project.dependency.resolution = {
         compile(':spring-security-core:2.0-RC5')
         compile ":rest-client-builder:2.1.1"
         compile (":twitter-bootstrap:3.2.0.2"){excludes 'svn'}
-        compile ":cinnamon-db:3.1.1"
+        compile ":cinnamon-db:3.1.3"
         runtime ':tika-parser:1.3.0.1'
         compile ":remote-pagination:0.3"
         compile ":geb:${gebPluginVersion}"
