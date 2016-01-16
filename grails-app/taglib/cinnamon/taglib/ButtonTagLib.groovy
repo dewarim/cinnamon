d6bf098efdb7692ef6c7b7200c127079e356cfe3
ingo.wiarda <ingo_wiarda@dewarim.de>
2016-01-16 11:22:19 +0100

    Formatting

--- grails-app/taglib/cinnamon/taglib/ButtonTagLib.groovy
+++ grails-app/taglib/cinnamon/taglib/ButtonTagLib.groovy
@@ -1,22 +1,25 @@
 package cinnamon.taglib
+
 class ButtonTagLib {
-    
+
     /**
      * Display a link to the start page.
      */
     def homeButton = { attrs, body ->
-		out << """<span class="menuButton"><a class="home" href="${createLink(action:grailsApplication.config.defaultAction ?: 'index', controller: grailsApplication.config.defaultController ?: 'folder')}">""" << body() << "</a></span>"
-	}
+        out << """<span class="menuButton"><a class="home" href="${
+            createLink(action: grailsApplication.config.defaultAction ?: 'index', controller: grailsApplication.config.defaultController ?: 'folder')
+        }">""" << body() << "</a></span>"
+    }
 
     /**
      * Display a link to the main administration page.
      */
     def adminButton = { attrs ->
-		out << """<span class="menuButton">
-        <a class="administration" href="${createLink(action:'index', controller: 'admin')}">
-        ${message(code:"link.to.administration")}
+        out << """<span class="menuButton">
+        <a class="administration" href="${createLink(action: 'index', controller: 'admin')}">
+        ${message(code: "link.to.administration")}
         </a>
         </span>"""
-	}
-    
+    }
+
 }
