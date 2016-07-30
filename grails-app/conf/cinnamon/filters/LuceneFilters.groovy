d90d1149dd071c6fa043f9fcc58cdece424afe2d
ingo.wiarda <ingo_wiarda@dewarim.de>
2016-07-30 10:22:34 +0200

    If the standard indexing fails with an exception, we try to add the objects to the IndexJob queue so they will be indexed asynchronously.
    Hope this fixes #118

--- grails-app/conf/cinnamon/filters/LuceneFilters.groovy
+++ grails-app/conf/cinnamon/filters/LuceneFilters.groovy
@@ -3,6 +3,7 @@ package cinnamon.filters
 import cinnamon.LocalRepository
 import cinnamon.ObjectSystemData
 import cinnamon.index.IndexAction
+import cinnamon.index.IndexJob
 import cinnamon.index.Indexable
 
 class LuceneFilters {
@@ -25,16 +26,30 @@ class LuceneFilters {
                     log.debug("*** After View-Exception ***",e)
                 }
                 else{
-                    ObjectSystemData.withNewTransaction {                        
-                        Map<Indexable, IndexAction> updatedObjects = LocalRepository.getUpdatedObjects();
-                        for(Indexable indexable : updatedObjects.keySet()){
-                            log.debug("Working on indexable #"+indexable.myId());
-                            switch(  updatedObjects.get(indexable)){
-                                case IndexAction.ADD: luceneService.addToIndex(indexable);break;
-                                case IndexAction.UPDATE: luceneService.updateIndex(indexable);break;
-                                case IndexAction.REMOVE : luceneService.removeFromIndex(indexable);break;
+                    try {
+                        ObjectSystemData.withNewTransaction {
+                            Map<Indexable, IndexAction> updatedObjects = LocalRepository.getUpdatedObjects();
+                            for (Indexable indexable : updatedObjects.keySet()) {
+                                log.debug("Working on indexable #" + indexable.myId());
+                                switch (updatedObjects.get(indexable)) {
+                                    case IndexAction.ADD: luceneService.addToIndex(indexable); break;
+                                    case IndexAction.UPDATE: luceneService.updateIndex(indexable); break;
+                                    case IndexAction.REMOVE: luceneService.removeFromIndex(indexable); break;
+                                }
                             }
                         }
+                    }
+                    catch (Exception luceneFail){
+                        log.warn("Indexing new objects failed (Stacktrace follows), will try to recover",e)
+                        Map<Indexable, IndexAction> updatedObjects = LocalRepository.getUpdatedObjects();
+                        for (Indexable indexable : updatedObjects.keySet()) {
+                            log.debug("Adding indexable #" + indexable.myId() + " to list objects which Lucene should try to update again.");
+                            IndexJob.withTransaction {
+                                def job = new IndexJob(indexable)
+                                job.save()
+                            }
+                        }
+                        log.info("Added ${updatedObjects.size()} objects to IndexJob queue.")
                     }
                 }
                 LocalRepository.cleanUp()
