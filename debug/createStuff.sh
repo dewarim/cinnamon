a3ea31ef9da85d56338c19accd45cf86188cb61b
ingo.wiarda <ingo_wiarda@dewarim.de>
2016-08-16 22:47:18 +0200

    Add searchobjects query to load testing script.

--- debug/createStuff.sh
+++ debug/createStuff.sh
@@ -3,9 +3,15 @@
 # debugging script: create many objects, use in multiple terminals to provoke concurrency problems.
 
 export TICKET="ddf02ffd-f6f4-4cc5-817e-d022c6818049@demo"
+export QUERY="<BooleanQuery><Clause+occurs='must'><TermQuery+fieldName='name'>test<%2FTermQuery><%2FClause><%2FBooleanQuery>"
+
+
 set -e
 
 for i in `seq 1 1000`;
 do
-    curl --header "ticket:${TICKET}" -d preid=15111 http://localhost:8080/cinnamon/cinnamon/legacy?command=version
+    curl --header "ticket:${TICKET}" -d preid=12554 http://localhost:8080/cinnamon/cinnamon/legacy?command=version
+    echo ""
+    curl --header "ticket:${TICKET}" -d query=${QUERY} http://localhost:8080/cinnamon/cinnamon/legacy?command=searchobjects > /tmp/searchResult.xml
+    echo ""
 done
