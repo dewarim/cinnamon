63809ddab9cdcfb09728a6988672a7be87ae6a8e
ingo.wiarda <ingo_wiarda@dewarim.de>
2018-06-30 12:41:31 +0200

    Editing of IndexTypes now works.
    Fix #153

--- grails-app/controllers/cinnamon/IndexTypeController.groovy
+++ grails-app/controllers/cinnamon/IndexTypeController.groovy
@@ -9,138 +9,145 @@ import cinnamon.index.IndexItem
 import grails.util.Holders
 
 @Secured(["hasRole('_superusers')"])
-class IndexTypeController extends BaseController{
+class IndexTypeController extends BaseController {
 
     static allowedMethods = [save: "POST", update: "POST", delete: "POST"]
 
-    def index () {
+    def index() {
         redirect(action: "list", params: params)
     }
 
-    def list () {
+    def list() {
         setListParams()
         [indexTypeList: IndexType.list(params)]
     }
 
-    def create () {
+    def create() {
         return [
-                indexers : Holders.config.indexers,
-                valueAssistanceProviders : Holders.config.vaProviders,
+                indexers                : Holders.config.indexers,
+                valueAssistanceProviders: Holders.config.vaProviders,
         ]
     }
 
-    def save () {
-        def saveModel = [  indexers : Holders.config.indexers,
-                valueAssistanceProviders : Holders.config.vaProviders]
-    	if (params.name.length() == 0) {
-    		flash.error = message(code: 'error.create.indexType.empty.name')
-    		render(view: "create", model: saveModel)
-    		return
-    	}
-    	if (params.indexerClass.length() == 0) {
-    		flash.error = message(code: 'error.create.indexType.empty.indexerClass')
-    		render(view: "create", model: saveModel)
-    		return
-    	}
-    	if (params.vaProviderClass.length() == 0) {
-    		flash.error = message(code: 'error.create.indexType.empty.vaProviderClass')
-    		render(view: "create", model: saveModel)
-    		return
-    	}
-    	if (IndexType.findByName(params.name)) {
-    		flash.error = message(code: 'error.create.duplicate.name', args: [message(code:'indexType.label'), message(code:'indexType.name.label'), params.name?.encodeAsHTML()])
-    		render(view: "create", model: saveModel)
-    		return
-    	}
+    def save() {
+        def saveModel = [indexers                : Holders.config.indexers,
+                         valueAssistanceProviders: Holders.config.vaProviders]
+        if (params.name.length() == 0) {
+            flash.error = message(code: 'error.create.indexType.empty.name')
+            render(view: "create", model: saveModel)
+            return
+        }
+        if (params.indexerClass.length() == 0) {
+            flash.error = message(code: 'error.create.indexType.empty.indexerClass')
+            render(view: "create", model: saveModel)
+            return
+        }
+        if (params.vaProviderClass.length() == 0) {
+            flash.error = message(code: 'error.create.indexType.empty.vaProviderClass')
+            render(view: "create", model: saveModel)
+            return
+        }
+        if (IndexType.findByName(params.name)) {
+            flash.error = message(code: 'error.create.duplicate.name', args: [message(code: 'indexType.label'), message(code: 'indexType.name.label'), params.name?.encodeAsHTML()])
+            render(view: "create", model: saveModel)
+            return
+        }
 
-    	String name = params.name
-    	try {
-	    	Class<Indexer> indexer = (Class<Indexer>) Class.forName(params.indexerClass - 'class ', true, Thread.currentThread().contextClassLoader)
-	    	Class<ValueAssistanceProvider> vaProviderClass = 
-                (Class<ValueAssistanceProvider>) Class.forName(params.vaProviderClass - 'class ', true, Thread.currentThread().contextClassLoader )
-	    	DataType dataType = Enum.valueOf(DataType, params.dataType)
-	    	IndexType indexTypeInstance = new IndexType(name, indexer, vaProviderClass, dataType)
-	
-	        if (indexTypeInstance.save(flush: true)) {
-	            flash.message = "${message(code: 'default.created.message', args: [message(code: 'indexType.label', default: 'IndexType'), indexTypeInstance.id])}"
-	            redirect(action: "show", id: indexTypeInstance.id)
-	        }
-	        else {
-	            render(view: "create", model: [indexTypeInstance: indexTypeInstance, 
-                        indexers : Holders.config.indexers,
-                        valueAssistanceProviders : Holders.config.vaProviders, ])
-	        }
-    	} catch (ClassNotFoundException ex) {
+        String name = params.name
+        try {
+            Class<Indexer> indexer = (Class<Indexer>) Class.forName(params.indexerClass - 'class ', true, Thread.currentThread().contextClassLoader)
+            Class<ValueAssistanceProvider> vaProviderClass =
+                    (Class<ValueAssistanceProvider>) Class.forName(params.vaProviderClass - 'class ', true, Thread.currentThread().contextClassLoader)
+            DataType dataType = Enum.valueOf(DataType, params.dataType)
+            IndexType indexTypeInstance = new IndexType(name, indexer, vaProviderClass, dataType)
+
+            if (indexTypeInstance.save(flush: true)) {
+                flash.message = "${message(code: 'default.created.message', args: [message(code: 'indexType.label', default: 'IndexType'), indexTypeInstance.id])}"
+                redirect(action: "show", id: indexTypeInstance.id)
+            } else {
+                render(view: "create", model: [indexTypeInstance       : indexTypeInstance,
+                                               indexers                : Holders.config.indexers,
+                                               valueAssistanceProviders: Holders.config.vaProviders,])
+            }
+        } catch (ClassNotFoundException ex) {
             log.debug("ClassNotFound: ", ex);
-    		flash.error = message(code:'class.not.found', args: [ex?.cause?.message])
-    		render(view: "create", model: saveModel)
-    	}
+            flash.error = message(code: 'class.not.found', args: [ex?.cause?.message])
+            render(view: "create", model: saveModel)
+        }
     }
 
-    def show () {
+    def show() {
 //        IndexType foo = new IndexType();
 //        log.debug(foo)
 //        foo = IndexType.get(62)
         def indexTypeInstance = null
         try {
             indexTypeInstance = IndexType.get(params.id)
-            if (! indexTypeInstance) {
+            if (!indexTypeInstance) {
                 flash.message = "${message(code: 'default.not.found.message', args: [message(code: 'indexType.label', default: 'IndexType'), params.id])}"
                 return redirect(action: "list")
             }
         }
         catch (Exception e) {
-            log.debug("failed to display IndexType:",e)
+            log.debug("failed to display IndexType:", e)
         }
-        
-        return [indexTypeInstance:indexTypeInstance,
-            deleteAllowed: IndexItem.findAll("from IndexItem as item where item.indexType=?", [indexTypeInstance])?.isEmpty()
-            //IndexItem.findByIndexType(indexTypeInstance)
+
+        return [indexTypeInstance: indexTypeInstance,
+                deleteAllowed    : IndexItem.findAll("from IndexItem as item where item.indexType=?", [indexTypeInstance])?.isEmpty()
+                //IndexItem.findByIndexType(indexTypeInstance)
         ]
     }
 
-    def edit () {
+    def edit() {
         def indexTypeInstance = IndexType.get(params.id)
         if (!indexTypeInstance) {
             flash.message = "${message(code: 'default.not.found.message', args: [message(code: 'indexType.label', default: 'IndexType'), params.id])}"
             return redirect(action: "list")
-        }
-        else {
-            return [indexTypeInstance: indexTypeInstance,
-                    indexers : Holders.config.indexers,
-                    valueAssistanceProviders : Holders.config.vaProviders,
+        } else {
+            return [indexTypeInstance       : indexTypeInstance,
+                    indexers                : Holders.config.indexers,
+                    dataTypeValues          : DataType.values(),
+                    valueAssistanceProviders: Holders.config.vaProviders,
             ]
         }
     }
 
-    def update () {
+    def update() {
         def indexTypeInstance = IndexType.get(params.id)
         if (indexTypeInstance) {
-            if (params.obj_version) {
-                def obj_version = params.obj_version.toLong()
-                if (indexTypeInstance.version > obj_version) {
-                    
-                    indexTypeInstance.errors.rejectValue("obj_version", "default.optimistic.locking.failure", [message(code: 'indexType.label', default: 'IndexType')] as Object[], "Another user has updated this IndexType while you were editing")
-                    render(view: "edit", model: [indexTypeInstance: indexTypeInstance])
-                    return
-                }
-            }
             indexTypeInstance.properties = params
+            try {
+                log.debug("looking for class: " + params.indexerClassName)
+                Class<Indexer> indexer = (Class<Indexer>) Class.forName(params.indexerClassName, true, Thread.currentThread().contextClassLoader)
+                log.debug("looking for class: " + params.vaProviderClassName)
+                Class<ValueAssistanceProvider> vaProviderClass =
+                        (Class<ValueAssistanceProvider>) Class.forName(params.vaProviderClassName, true, Thread.currentThread().contextClassLoader)
+                indexTypeInstance.indexerClass = indexer
+                indexTypeInstance.vaProviderClass = vaProviderClass
+            } catch (ClassNotFoundException ex) {
+                log.debug("ClassNotFound: ", ex);
+                flash.error = message(code: 'class.not.found', args: [ex?.cause?.message])
+                render(view: "edit", model: [indexTypeInstance: indexTypeInstance])
+                return
+            }
+            catch(Exception e){
+                log.warn("Failed to load classes",e)
+                throw new RuntimeException(e)
+            }
+
             if (!indexTypeInstance.hasErrors() && indexTypeInstance.save(flush: true)) {
                 flash.message = "${message(code: 'default.updated.message', args: [message(code: 'indexType.label', default: 'IndexType'), indexTypeInstance.id])}"
                 redirect(action: "show", id: indexTypeInstance.id)
-            }
-            else {
+            } else {
                 render(view: "edit", model: [indexTypeInstance: indexTypeInstance])
             }
-        }
-        else {
+        } else {
             flash.message = "${message(code: 'default.not.found.message', args: [message(code: 'indexType.label', default: 'IndexType'), params.id])}"
             redirect(action: "list")
         }
     }
 
-    def delete () {
+    def delete() {
         def indexTypeInstance = IndexType.get(params.id)
         if (indexTypeInstance) {
             try {
@@ -152,16 +159,15 @@ class IndexTypeController extends BaseController{
                 flash.message = "${message(code: 'default.not.deleted.message', args: [message(code: 'indexType.label', default: 'IndexType'), params.id])}"
                 redirect(action: "show", id: params.id)
             }
-        }
-        else {
+        } else {
             flash.message = "${message(code: 'default.not.found.message', args: [message(code: 'indexType.label', default: 'IndexType'), params.id])}"
             redirect(action: "list")
         }
     }
 
-    def updateList () {
+    def updateList() {
         setListParams()
-        render(template: 'indexTypeList', model:[indexTypeList:IndexType.list(params)])
+        render(template: 'indexTypeList', model: [indexTypeList: IndexType.list(params)])
     }
 
 }
--- grails-app/views/indexType/edit.gsp
+++ grails-app/views/indexType/edit.gsp
@@ -1,76 +1,84 @@
 <!DOCTYPE HTML>
 <html>
 <head>
-	<meta name="layout" content="main"/>
+    <meta name="layout" content="main"/>
 
-        <g:set var="entityName" value="${message(code: 'indexType.label')}" />
-        <title><g:message code="indexType.edit.label" args="[entityName]" /></title>
-    </head>
-    <body>
-          <div class="nav">
-			<g:homeButton><g:message code="home"/></g:homeButton><g:adminButton/>
-            <span class="menuButton"><g:link class="list" action="list"><g:message code="indexType.list.label" args="[entityName]" /></g:link></span>
-            <span class="menuButton"><g:link class="create" action="create"><g:message code="indexType.new.label" args="[entityName]" /></g:link></span>
+    <g:set var="entityName" value="${message(code: 'indexType.label')}"/>
+    <title><g:message code="indexType.edit.label" args="[entityName]"/></title>
+</head>
+
+<body>
+<div class="nav">
+    <g:homeButton><g:message code="home"/></g:homeButton><g:adminButton/>
+    <span class="menuButton"><g:link class="list" action="list"><g:message code="indexType.list.label"
+                                                                           args="[entityName]"/></g:link></span>
+    <span class="menuButton"><g:link class="create" action="create"><g:message code="indexType.new.label"
+                                                                               args="[entityName]"/></g:link></span>
+</div>
+
+<div class="content">
+    <h1><g:message code="indexType.edit.label" args="[entityName]"/></h1>
+    <g:render template="/shared/message"/>
+
+    <g:hasErrors bean="${indexTypeInstance}">
+        <div class="errors">
+            <g:renderErrors bean="${indexTypeInstance}" as="list"/>
         </div>
-        <div class="content">
-            <h1><g:message code="indexType.edit.label" args="[entityName]" /></h1>
-			<g:render template="/shared/message"/>
-			
-            <g:hasErrors bean="${indexTypeInstance}">
-            <div class="errors">
-                <g:renderErrors bean="${indexTypeInstance}" as="list" />
-            </div>
-            </g:hasErrors>
-            <g:form method="post" >
-                <g:hiddenField name="id" value="${indexTypeInstance?.id}" />
-                <div class="dialog">
-                    <table>
-                        <tbody>
-                        
-                            <tr class="prop">
-                                <td class="name">
-                                  <label for="name"><g:message code="indexType.name.label" /></label>
-                                </td>
-                                <td class="value ${hasErrors(bean: indexTypeInstance, field: 'name', 'errors')}">
-                                    <g:textField name="name" value="${indexTypeInstance?.name}" />
-                                </td>
-                            </tr>
-                        
-                            <tr class="prop">
-                                <td class="name">
-                                  <label for="dataType"><g:message code="indexType.dataType.label" /></label>
-                                </td>
-                                <td class="value ${hasErrors(bean: indexTypeInstance, field: 'dataType', 'errors')}">
-                                    <g:select name="dataType" from="${cinnamon.index.IndexType$DataType?.values()}" value="${indexTypeInstance?.dataType}"  />
-                                </td>
-                            </tr>
-                        
-                            <tr class="prop">
-                                <td class="name">
-                                  <label for="indexerClass"><g:message code="indexType.indexerClass.label" /></label>
-                                </td>
-                                <td class="value ${hasErrors(bean: indexTypeInstance, field: 'indexerClass', 'errors')}">
-                                    <g:select name="indexerClass" from="${indexers}" value="${indexTypeInstance?.indexerClass?.name}" />
-                                </td>
-                            </tr>
-                        
-                            <tr class="prop">
-                                <td class="name">
-                                  <label for="vaProviderClass"><g:message code="indexType.vaProviderClass.label" /></label>
-                                </td>
-                                <td class="value ${hasErrors(bean: indexTypeInstance, field: 'vaProviderClass', 'errors')}">
-									<g:select id="vaProviderClass" name="valueAssistanceProvider" from="${valueAssistanceProviders}" value="${indexTypeInstance?.vaProviderClass?.name}" />
-                                </td>
-                            </tr>
-                        
-                        </tbody>
-                    </table>
-                </div>
-                <div class="buttons">
-                    <span class="button"><g:actionSubmit class="save" action="update" value="${message(code: 'default.button.update.label')}" /></span>
-                </div>
-            </g:form>
+    </g:hasErrors>
+    <g:form method="post">
+        <g:hiddenField name="id" value="${indexTypeInstance?.id}"/>
+        <div class="dialog">
+            <table>
+                <tbody>
+
+                <tr class="prop">
+                    <td class="name">
+                        <label for="name"><g:message code="indexType.name.label"/></label>
+                    </td>
+                    <td class="value ${hasErrors(bean: indexTypeInstance, field: 'name', 'errors')}">
+                        <g:textField name="name" value="${indexTypeInstance?.name}"/>
+                    </td>
+                </tr>
+
+                <tr class="prop">
+                    <td class="name">
+                        <label for="dataType"><g:message code="indexType.dataType.label"/></label>
+                    </td>
+                    <td class="value ${hasErrors(bean: indexTypeInstance, field: 'dataType', 'errors')}">
+                        <g:select name="dataType" from="${dataTypeValues}"
+                                  value="${indexTypeInstance?.dataType}"/>
+                    </td>
+                </tr>
+
+                <tr class="prop">
+                    <td class="name">
+                        <label for="indexerClass"><g:message code="indexType.indexerClass.label"/></label>
+                    </td>
+                    <td class="value ${hasErrors(bean: indexTypeInstance, field: 'indexerClass', 'errors')}">
+                        <g:select name="indexerClassName" from="${indexers}"
+                                  value="${indexTypeInstance?.indexerClass?.name}"/>
+                    </td>
+                </tr>
+
+                <tr class="prop">
+                    <td class="name">
+                        <label for="vaProviderClass"><g:message code="indexType.vaProviderClass.label"/></label>
+                    </td>
+                    <td class="value ${hasErrors(bean: indexTypeInstance, field: 'vaProviderClass', 'errors')}">
+                        <g:select id="vaProviderClassName" name="vaProviderClassName" from="${valueAssistanceProviders}"
+                                  value="${indexTypeInstance?.vaProviderClass?.name}"/>
+                    </td>
+                </tr>
+
+                </tbody>
+            </table>
         </div>
 
+        <div class="buttons">
+            <span class="button"><g:actionSubmit class="save" action="update"
+                                                 value="${message(code: 'default.button.update.label')}"/></span>
+        </div>
+    </g:form>
+</div>
 
 </body></html>
